da0da04f2fc07016179f9fd2c5a6151eb73f79c1
Yunlincla <k5711628@gmail.com>
2024-10-15 11:29:53 +0800

    LoadableVIew明確錯誤處理。

--- YetAnotherIVOD/LoadableView.swift
+++ YetAnotherIVOD/LoadableView.swift
@@ -6,38 +6,49 @@
 //
 
 import SwiftUI
+import LYFetcher
 
 struct LoadableView<Value, Content>: View where Content: View {
     @Binding var value: Value?
-    var loader: () async -> Value
+    var loader: () async throws -> Value
     var content: (Value) -> Content
+    @State private var isError = false
+    
     var body: some View {
         if let value {
             content(value)
         } else {
-            ProgressView("加載中")
-                .onAppear {
-                    Task {
-                        value = await loader()
-                    }
+            if !isError {
+                ProgressView("加載中")
+                    .onAppear(perform: Load)
+            } else {
+                VStack {
+                    ContentUnavailableView("連線錯誤", systemImage: "network.slash", description: Text("請檢查網路連線！\n若此問題持續發生，請聯絡開發者！"))
+                    Button("點我嘗試重新整理", action: Load)
+                        .padding()
+                    // 提示使用者缺乏網路連線
+                    // 或解析錯誤，請聯絡開發者
                 }
-        }
-    }
-}
-
-private struct TestLoadableView: View {
-    @State var str: String?
-    var body: some View {
-        LoadableView(value: $str, loader: Loader) {
-            Text($0)
+            }
         }
     }
     
-    func Loader() -> String {
-        return "!"
+    func Load() {
+        isError = false
+        Task {
+            do {
+                value = try await loader()
+                isError = false
+            } catch {
+                isError = true
+            }
+        }
     }
 }
 
 #Preview("LoadableView") {
-    TestLoadableView()
+    MeetingList(selectedVideo: .constant(nil))
+#if os(macOS)
+        .frame(width: 400, height: 400)
+#endif
 }
